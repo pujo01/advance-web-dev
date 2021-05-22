f5972aa19bd183c6d685fbf7906a89baa31ecaf2
Yose Alloisius <50135810+yoseas@users.noreply.github.com>
2021-05-23 05:29:33 +0700

    Update RegistrationForm.jsx

--- src/RegistrationForm.jsx
+++ src/RegistrationForm.jsx
@@ -9,7 +9,7 @@ import { Input, Button } from 'react-advanced-form-addons'
 
 export default class RegistrationForm extends React.Component {
   registerUser = ({ serialized, fields, form }) => {
-    return fetch('https://advance-web-dev-3-2htyjowz4-yoseas.vercel.app', {
+    return fetch('https://advance-web-dev-3.vercel.app/', {
       body: JSON.stringify(serialized)
     })
   }
